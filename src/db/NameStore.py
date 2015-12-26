ad77130e03b42d42a04ea81f83aff03c6cf928d3
Georg Vogelhuber <jgvogelhuber@gmail.com>
2015-12-26 18:04:42 +0100

    Basic implementation for NameStore

--- src/db/NameStore.py
+++ src/db/NameStore.py
@@ -1,3 +1,4 @@
+import sqlite3
 
 DB_PATH = "names.db"
 
@@ -5,8 +6,32 @@ class NameStore(object):
 
     def __init__(self):
         self.db_path = DB_PATH
+        self.connection = sqlite3.connect(self.db_path)
+        self.cursor = self.connection.cursor()
 
-    def get_entry(name):
-        pass
+    def __del__(self):
+        if self.connection:
+            self.connection.close()
 
-    def find
+    def get_entry_by_name(self, name):
+        try:
+            self.cursor.execute("SELECT * FROM NAMES WHERE NAME=?", [(name)])
+            data = self.cursor.fetchall()
+
+            return data
+        except sqlite3.Error:
+            pass
+
+    def get_unrated_entries(self):
+        try:
+            self.cursor.execute("SELECT * FROM NAMES WHERE RATING<0")
+            data = self.cursor.fetchall()
+
+            return data
+        except sqlite3.Error:
+            pass
+
+if __name__ == "__main__":
+    name_store = NameStore()
+    print name_store.get_unrated_entries()
+    print name_store.get_entry_by_name("Lara")
